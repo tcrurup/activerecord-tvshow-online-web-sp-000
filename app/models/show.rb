6ec5a9e40135acd87b771c88ce8eb37cb640dbe9
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 23:52:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -25,10 +25,6 @@ class Show < ActiveRecord::Base
   end
   
   def self.shows_by_alphabetical_order
-    #shows = []
-    Show.order(:name)#.each do |s|
-      #shows << s
-    #end
-    #shows
+    Show.order(:name)
   end
 end
